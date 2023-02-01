40befe746b6dc27f3421ec89703f36d639dc80cc
nanak <nanaksethh@gmail.com>
2023-02-01 14:13:22 +0530

    chnages in database

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'backend'),
-      user: env('DATABASE_USERNAME', 'nanak'),
-      password: env('DATABASE_PASSWORD', 'Nanak123seth'),
+      database: env('DATABASE_NAME', 'talensourcing-1'),
+      user: env('DATABASE_USERNAME', 'talensourcing-1'),
+      password: env('DATABASE_PASSWORD','talensourcing-1'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
